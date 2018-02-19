b9f6f6aa2dc1ca48734d6b285ca98d46b9629a67
Krasen Ivanov <krasen007@gmail.com>
2018-02-20 01:25:34 +0200

    init

---
+++ FirstRun.cs
@@ -0,0 +1,23 @@
+﻿namespace Start
+{
+    using System;
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class FirstRun
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            using (var game = new Start())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
