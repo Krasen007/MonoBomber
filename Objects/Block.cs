041d6a9935b49a407dcac9d89f2c9e105c1ad5a4
paphl <36862339+paphl@users.noreply.github.com>
2018-02-26 23:35:35 +0200

    Base of Block class
    Тука съм нахвърлил горе долу какво ще съдържа оснотния клас за блокове.
    Ако се сетите за нещо кажете или добавете напр :+1:

--- Objects/Block.cs
+++ Objects/Block.cs
@@ -1,12 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MonoContra.Objects
+﻿namespace MonoContra.Objects
 {
+    using global::MonoContra.Utilities;
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Content;
+    using Microsoft.Xna.Framework.Graphics;
+    using System.Collections.Generic;
+
     class Block
     {
+        protected Dictionary<string, Animation> animations;
+        protected string currentAnimationKey;
+
+        protected double velocity;
+        protected Vector2 position;
+        protected double rotation;
+        protected Vector2 scale;
+
+        public bool isBreakable { get; protected set; }
+        public Vector2 Position
+        {
+            get
+            {
+                return this.position;
+            }
+        }
+
+        public Block(ContentManager Content, int gameWidth, int gameHeight, double velocity, Vector2 scale)
+        {
+            this.velocity = velocity;
+            this.scale = scale;
+            this.currentAnimationKey = "";
+            this.animations = new Dictionary<string, Animation>();
+
+            this.CreateAnimations(Content);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            this.animations[this.currentAnimationKey].Draw(spriteBatch, this.rotation, this.position, this.scale);
+        }
+
+        protected abstract void CreateAnimations(ContentManager Content);
     }
 }
