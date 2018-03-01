801a0072ba23e1e35df98fe763ae8cc36e78fbb7
Krasen Ivanov <krasen007@gmail.com>
2018-03-01 09:11:13 +0200

    Dont push broken games

--- Objects/Wall.cs
+++ Objects/Wall.cs
@@ -7,26 +7,28 @@
     using global::MonoContra.Utilities;
     using global::MonoContra.Objects;
     using MonoContra.Enumarables;
+
     //draw
     //isAlive
     //isDestryctable
     public class Wall : Block
     {
-
         private const string UNBREAKABLE_ANIMATION_KEY = "Tree";
         private const string BREAKABLE_ANIMATION_KEY = "wall";
 
         public WallTypes WallType { get; set; }
-        public Wall(ContentManager content,bool health, WallTypes wallType, int gameWidth, int gameHeight, double velocity, Vector2 scale)
-            :base(content, health, gameWidth, gameHeight, velocity, scale)
+
+        public Wall(ContentManager content, bool health, WallTypes wallType, int gameWidth, int gameHeight, double velocity, Vector2 scale)
+            : base(content, health, gameWidth, gameHeight, velocity, scale)
         {
             this.Position = new Vector2(100, 100);
             this.WallType = wallType;
             this.CurrentAnimationKey = UNBREAKABLE_ANIMATION_KEY;
         }
+
         public void Update(GameTime gameTime, int gameWidth, int gameHeight)
         {
-           foreach(var pair in this.animations)
+            foreach (var pair in this.Animations)
             {
                 pair.Value.Update(gameTime);
             }
@@ -37,8 +39,7 @@
             var unbreakableWall = content.Load<Texture2D>(UNBREAKABLE_ANIMATION_KEY);
             var breakableWall = content.Load<Texture2D>(BREAKABLE_ANIMATION_KEY);
 
-
-            this.animations.Add(UNBREAKABLE_ANIMATION_KEY, new Animation(unbreakableWall,1, 1,1,239, 286)); //gameHeight / (wallPerRow * 2)
+            this.Animations.Add(UNBREAKABLE_ANIMATION_KEY, new Animation(unbreakableWall, 1, 1, 1, 239, 286)); // gameHeight / (wallPerRow * 2)
             //this.animations.Add(BREAKABLE_ANIMATION_KEY, new Animation(breakableWall, 1, 1, 1, 50, 50));
         }
     }
