fa3332560a20abecbb31bbda688142e74a6115a3
unknown <treneti@abv.bg>
2018-03-02 00:27:51 +0200

    added class power ups

---
+++ Objects/PowerUp.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace MonoContra.Objects
+{
+    using System;
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Content;
+
+    class PowerUp : Block
+    {
+        public PowerUp(ContentManager content, Vector2 position, bool health, int gameWidth, int gameHeight, double velocity, Vector2 scale) : base(content, position, health, gameWidth, gameHeight, velocity, scale)
+        {
+
+        }
+
+        protected override void CreateAnimations(ContentManager content)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
