be0dfed884b5d8478d2e23673c76587a560e3bf2
unknown <treneti@abv.bg>
2018-03-02 23:07:56 +0200

    powerups

--- Enumerables/PowerUpTypes.cs
+++ Enumerables/PowerUpTypes.cs
@@ -5,6 +5,6 @@
         BombSatchel,        // +1 bomb
         Sonic,              // +3 speed       
         DragonLance,        // +1 range
-        MarioStrar          // can go through walls instantly kill enemy you pass through 
+        MarioStrar          // can go through walls and instantly kill enemy you pass through 
     }
 }
