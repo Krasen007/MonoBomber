1bc3ca76e384016f89d6c6dedbe9da37970859b5
unknown <treneti@abv.bg>
2018-03-02 22:27:33 +0200

    power ups enum added

---
+++ Enumerables/PowerUpTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace MonoContra.Enumerables
+{
+    public enum PowerUpTypes
+    {
+        BombSatchel,        // +1 bomb
+        Sonic,              // +3 speed       
+        DragonLance,        // +1 range
+        MarioStrar          // can go through walls instantly kill enemy you pass through 
+    }
+}
